82b2a117eeb3e663c6994f6ca4117a439de5a574
ShahByte <158125041+ShahByte@users.noreply.github.com>
2025-10-06 00:24:53 +0800

    config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser(description='PyTorch Unsupervised Segmentation'
 parser.add_argument('--inChannel', default=100,  type=int)
 parser.add_argument('--midChannel', default=100, type=int)
 parser.add_argument('--outClust',  default=100, type=int)
+parser.add_argument('--in_shape_y',  default=70, type=int)
+parser.add_argument('--in_shape_x',  default=70, type=int)
 parser.add_argument('--maxIter', default=300, type=int)
 parser.add_argument('--minLabels',  default=4, type=int)
 parser.add_argument('--lr',  default=0.01, type=float)
@@ -13,3 +15,4 @@ parser.add_argument('--model_dir', metavar='epoch dir', default='model_weights/'
                     help='File path', required=False)
 
 args = parser.parse_args()
+
